546123ab06d80fc45b6f33d78478de08b3a14f51
ay63 <74818855+ay63@users.noreply.github.com>
2025-01-23 10:14:44 +0100

    test: add session e2e

--- front/cypress/e2e/admin/session.cy.ts
+++ front/cypress/e2e/admin/session.cy.ts
@@ -2,6 +2,60 @@ import * as cypress from "cypress";
 
 beforeEach(() => {
   cy.visit('/login')
+
+  cy.intercept('POST', '/api/auth/login', {
+    body: {
+      id: 1,
+      username: 'userName',
+      firstName: 'firstName',
+      lastName: 'lastName',
+      admin: true
+    },
+    statusCode: 200
+  })
+
+  cy.intercept('GET', '/api/session', {
+    body: [
+      {
+        "id": 1,
+        "name": "test",
+        "date": "2025-01-22T00:00:00.000+00:00",
+        "teacher_id": 1,
+        "description": "test",
+        "users": [],
+        "createdAt": "2025-01-23T09:12:33",
+        "updatedAt": "2025-01-23T09:12:33"
+      },
+    ],
+    statusCode: 200
+  })
+
+  cy.intercept('GET', '/api/teacher', {
+    body: [
+      {
+        "id": 1,
+        "lastName": "DELAHAYE",
+        "firstName": "Margot",
+        "createdAt": "2025-01-20T10:26:21",
+        "updatedAt": "2025-01-20T10:26:21"
+      },
+    ],
+    statusCode: 200
+  })
+
+  cy.intercept('GET', '/api/session/1', {
+    body: {
+      "id": 1,
+      "name": "test",
+      "date": "2025-01-22T00:00:00.000+00:00",
+      "teacher_id": 1,
+      "description": "test",
+      "users": [],
+      "createdAt": "2025-01-23T09:12:33",
+      "updatedAt": "2025-01-23T09:12:33"
+    },
+  })
+
   cy.get('input[formcontrolname=email]').type(Cypress.env('adminEmail'));
   cy.get('input[formcontrolname=password]').type(`${Cypress.env('adminPassword')}{enter}{enter}`)
 
@@ -12,31 +66,42 @@ beforeEach(() => {
 
 describe('Admin session', () => {
 
+  describe('Session create', () => {
+    it('Should be able to create a session', () => {
+      cy.getByDataCy("create-session").click()
+      cy.url().should('include', '/create')
 
-  it('Should be able to create a session', () => {
-    cy.getByDataCy("create-session").click()
-    cy.url().should('include', '/create')
+      cy.intercept('POST', '/sessions/create', {
+        body: {
+          "name": "session 1",
+          "date": "2012-01-01",
+          "teacher_id": 1,
+          "users": null,
+          "description": "my description"
+        },
+        statusCode: 200
+      })
 
-    cy.getByDataCy("name").type('test')
-    cy.getByDataCy("date").type('2025-01-22')
+      cy.getByDataCy("name").type('test')
+      cy.getByDataCy("date").type('2025-01-22')
+      cy.getByDataCy("teacher-select").click();
+      cy.getByDataCy("teacher-option").should('have.length.greaterThan', 0);
+      cy.getByDataCy("teacher-option").first().click()
+      cy.getByDataCy("description").type('a'.repeat(20))
 
-    cy.getByDataCy("teacher-select").click();
-    cy.getByDataCy("teacher-option").should('have.length.greaterThan', 0);
-    cy.getByDataCy("teacher-option").first().click()
-    cy.getByDataCy("description").type('a'.repeat(20))
-
-    cy.getByDataCy("saveBtn").click()
-    cy.url().should('include', '/sessions')
+      cy.getByDataCy("saveBtn").click()
+      cy.url().should('include', '/sessions')
+    })
   })
 
-
   it('Should be able to delete a session', () => {
-    let dataCy;
-    let match;
+    cy.intercept('DELETE', '/api/session', {
+      statusCode: 200
+    })
 
     cy.get('[data-cy^="detail-session-"]').first().then((btn) => {
-      dataCy = btn.attr('data-cy');
-      match = dataCy.match(/detail-session-(\d+)/);
+      const dataCy = btn.attr('data-cy');
+      const match = dataCy.match(/detail-session-(\d+)/);
 
       if (match) {
         const id = match[1];
@@ -49,12 +114,9 @@ describe('Admin session', () => {
   })
 
   it('Should be able to update a session', () => {
-    let dataCy;
-    let match;
-
     cy.get('[data-cy^="edit-session-"]').first().then((btn) => {
-      dataCy = btn.attr('data-cy');
-      match = dataCy.match(/edit-session-(\d+)/);
+      const dataCy = btn.attr('data-cy');
+      const match = dataCy.match(/edit-session-(\d+)/);
 
       if (match) {
         const id = match[1];
@@ -69,5 +131,34 @@ describe('Admin session', () => {
     cy.url().should('include', '/sessions')
   })
 
+  describe('Session create errors form on create and update ', () => {
+    it('Should not be able to create a session missing form data', () => {
+      cy.getByDataCy("create-session").click()
+      cy.url().should('include', '/create')
+      cy.getByDataCy("name").invoke('val', '')
+      cy.getByDataCy("date").invoke('val', '')
+      cy.getByDataCy("description").invoke('val', '')
+      cy.getByDataCy("saveBtn").should('be.disabled')
+
+      cy.getByDataCy("teacher-select").should('have.class', 'mat-select-required')
+      cy.getByDataCy("name").should('have.attr', 'required')
+      cy.getByDataCy("date").should('have.attr', 'required')
+      cy.getByDataCy("description").should('have.attr', 'required')
+
+      cy.url().should('include', '/create')
+    })
+
+    it('should failed description max length 2000', () => {
+      cy.getByDataCy("create-session").click()
+      cy.getByDataCy("name").type('test')
+      cy.getByDataCy("date").type('2025-01-22')
+      cy.getByDataCy("teacher-select").click();
+      cy.getByDataCy("teacher-option").should('have.length.greaterThan', 0);
+      cy.getByDataCy("teacher-option").first().click()
+      cy.getByDataCy("description").type('a'.repeat(2001))
+      cy.getByDataCy("description").should('have.class', 'ng-invalid')
+      cy.getByDataCy("saveBtn").should('be.disabled')
+    })
+  })
 
 })
